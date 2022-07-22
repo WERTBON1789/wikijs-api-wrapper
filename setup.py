c616ac991f68bf8ec4dde6e685abed380bb5352f
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-07-22 14:47:18 +0200

    Changed package structure and added example package to build

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wikijspy',
+    version='0.0.1',
+    author='WERTBON1789',
+    author_email='jan210404@gmx.de',
+    packages=find_packages(),
+    license='LICENSE',
+    description='A simple wikijs API wrapper',
+)
---
+++ wikijspy/__init__.py
@@ -0,0 +1,1 @@
+from wikijspy import test
