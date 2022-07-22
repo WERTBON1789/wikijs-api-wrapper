42944f8a5949f7cbe69167862801ef13cb6e65a3
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-07-22 20:37:02 +0200

    Added first unit test

--- tests/pages_list.py
+++ tests/pages_list.py
@@ -1,5 +1,14 @@
-import wikijspy
+from wikijspy import Configuration, ApiClient, PagesApi
+from wikijspy.types.page_types import PageListItemOutput
+import json
 
-wikijspy.Configuration()
+with open('tests/settings.json', 'r') as f:
+    settings_dict = json.loads(f.read())
+    WIKIJS_HOST = settings_dict["hostname"]
+    TOKEN = settings_dict["token"]
 
+conf = Configuration(WIKIJS_HOST, TOKEN)
 
+pages_api = PagesApi(ApiClient(conf))
+
+print(pages_api.list(PageListItemOutput(["title", "id", "path"])))
