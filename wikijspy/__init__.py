70687ae53e252b921a487f2d45e4c01919b04b6f
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-07-25 17:17:43 +0200

    Added all types to modules scope

--- wikijspy/__init__.py
+++ wikijspy/__init__.py
@@ -1,4 +1,5 @@
 from wikijspy.api_client import ApiClient
 from wikijspy.configuration import Configuration
 from wikijspy.api.pages import PagesApi
-from wikijspy.types.page_types import PageListItemOutput
+from wikijspy.types.page_types import *
+from wikijspy.types.general import *
