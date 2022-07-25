0589193b601ca27b8ae4abf27cff05f0861f7619
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-07-25 15:04:46 +0200

    Added page_update example

---
+++ examples/page_update.py
@@ -0,0 +1,30 @@
+from wikijspy import Configuration, PagesApi, ApiClient
+from wikijspy.types.page_types import PageResponseOutput
+
+import json
+
+
+
+with open('examples/settings.json', 'r') as f:
+    settings_dict = json.loads(f.read())
+    WIKIJS_HOST = settings_dict["hostname"]
+    TOKEN = settings_dict["token"]
+
+conf = Configuration(WIKIJS_HOST, TOKEN)
+
+pages_api = PagesApi(ApiClient(conf))
+
+print(pages_api.update(PageResponseOutput({
+    "responseResult": [
+        "message",
+        "errorCode"
+    ],
+    "page": [
+        "title",
+        "id"
+    ]
+}),
+    id=10637,
+    content="Hallo"
+))
+
