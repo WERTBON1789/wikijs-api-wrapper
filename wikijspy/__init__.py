e9c8012fd2ea215eaf94dc297652b31178592f08
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-07-27 21:09:51 +0200

    Added the AuthenticationApi to __init__.py

--- wikijspy/__init__.py
+++ wikijspy/__init__.py
@@ -2,6 +2,8 @@ from wikijspy.api_client import ApiClient
 from wikijspy.configuration import Configuration
 from wikijspy.api.pages import PagesApi
 from wikijspy.api.users import UsersApi
+from wikijspy.api.authentication import AuthenticationApi
 from wikijspy.types.page_types import *
 from wikijspy.types.users_types import *
+from wikijspy.types.authentication_types import *
 from wikijspy.types.general import *
