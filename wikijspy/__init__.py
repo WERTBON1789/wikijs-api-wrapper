152b0a2a40ca6db07f3cfd9f0be98da6d7fe38da
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-07-27 18:04:34 +0200

    Added UsersApi to __init__.py

--- wikijspy/__init__.py
+++ wikijspy/__init__.py
@@ -1,5 +1,7 @@
 from wikijspy.api_client import ApiClient
 from wikijspy.configuration import Configuration
 from wikijspy.api.pages import PagesApi
+from wikijspy.api.users import UsersApi
 from wikijspy.types.page_types import *
+from wikijspy.types.users_types import *
 from wikijspy.types.general import *
