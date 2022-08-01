1ab5c74786474cad9ab2cae0fb1b2db467a78561
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-08-01 15:51:37 +0200

    Added AuthenticationUserErrors enum

--- wikijspy/types/authentication_types.py
+++ wikijspy/types/authentication_types.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from wikijspy.types.general import Output
 
 
@@ -28,3 +29,26 @@ class AuthenticationActiveStrategyOutput(Output):
         "domainWhitelist",
         "autoEnrollGroups"
     ]
+
+class AuthenticationUserErrors(Enum):
+    Nothing = 0
+    AuthGenericError = 1001
+    AuthLoginFailed = 1002
+    AuthProviderInvalid = 1003
+    AuthAccountAlreadyExists = 1004
+    AuthTFAFailed = 1005
+    AuthTFAInvalid = 1006
+    BruteInstanceIsInvalid = 1007
+    BruteTooManyAttempts = 1008
+    UserCreationFailed = 1009
+    AuthRegistrationDisabled = 1010
+    AuthRegistrationDomainUnauthorized = 1011
+    InputInvalid = 1012
+    AuthAccountBanned = 1013
+    AuthAccountNotVerified = 1014
+    AuthValidationTokenInvalid = 1015
+    UserNotFound = 1016
+    UserDeleteForeignConstraint = 1017
+    UserDeleteProtected = 1018
+    AuthRequired = 1019
+    AuthPasswordInvalid = 1020
